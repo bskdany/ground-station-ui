67322ba3df1d5f0ca50b5459926aa27efda539b8
wildildee <jacobwilde@cmail.carleton.ca>
2024-02-10 01:50:44 -0500

    Modified Replay Button Text

--- src/components/replay/ReplayPopup.jsx
+++ src/components/replay/ReplayPopup.jsx
@@ -15,23 +15,13 @@ export default function ReplayPopup({status, websocketRef}) {
     
     // Check if we are currently playing the replay and set the button color accordingly
     let buttonState = status.status === 1 ? 'playing' : 'stopped';
-    
-    // Get the status icon for the replay status
-    let buttonText = "Replay ";
-    if (status.status === 1) {
-        // Playing
-        buttonText += "\u23F5";
-    } else {
-        // Stopped / Finished / No Replay Loaded
-        buttonText += "\u23F9";
-    }
 
     // Setup open state to have keybind
     const [open, setOpen] = useState(false);
     useKey("KeyR", "shift", () => {setOpen(!open)});
     return (
         <div>
-            <p className="replay-button" id={buttonState} onClick={() => {setOpen(!open)}}>{buttonText}</p>
+            <p className="replay-button" id={buttonState} onClick={() => {setOpen(!open)}}>{status.status === 1 ? "REPLAY: PLAY" : "REPLAY:STOP"}</p>
             <GenericPopup open={open} onClose={() => {setOpen(false)}} title="Replays" >
                 <div className="replay-popup-replaylist">
                     {replays}
